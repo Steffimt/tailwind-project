2cd63f13b3922197c72a60f9fd90e360e8d83376
Steffimt <steffimariatanasia@gmail.com>
2023-08-05 17:12:06 +0700

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        style: ['Inter'],
+        style2: ['Work Sans']
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+        custom: '8px 8px 15px rgba(0, 0, 0, 0.48)'
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
 
